4757b593bdd404b0db5c9c57f986902c3e5878db
youreallyshouldntreadthis <156702775+youreallyshouldntreadthis@users.noreply.github.com>
2026-04-05 21:38:37 +0300

    Update auto.sh
    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -4,9 +4,12 @@ ADS=("SXtm:EU-FRANKFURT-1-AD-1" "SXtm:EU-FRANKFURT-1-AD-2" "SXtm:EU-FRANKFURT-1-
 
 until [ "$n" -ge 300 ]
 do
-  AD=${ADS[$((n % 3))]}
+  AD=${ADS[$((n % ${#ADS[@]}))]}
   echo "[$(date)] Retry Attempt — $n | Trying AD: $AD"
   terraform apply -auto-approve -var="availability_domain=$AD" && exit 0
   n=$((n+1))
   sleep 60
 done
+
+echo "Couldnt create instance." >&2
+exit 1
